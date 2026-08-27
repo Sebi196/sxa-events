60476ef235b4f6e11fe4b9cbd30915655b6d1d32
Sebastian <sxaevents@yahoo.com>
2026-08-27 16:36:08 +0300

    Add Google Ads tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 
@@ -17,6 +18,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ro">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-1814357249"
+        />
+
+        <Script id="google-ads">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-1814357249');
+          `}
+        </Script>
+      </head>
+
       <body>
         <Navbar />
         {children}
