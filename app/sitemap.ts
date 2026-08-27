30d3d14692233788a66ad41cca486bc871c5dfa6
Sebi196 <sebigmng@yahoo.com>
2026-08-27 14:41:45 +0300

    Fix syntax error in sitemap function

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url:"https://www.sxaevents.ro",,
+      url: "https://www.sxaevents.ro",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
